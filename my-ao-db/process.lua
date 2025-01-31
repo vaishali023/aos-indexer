c90c480c419cdb39323647264d1b8e793bfc8900
Vaishali Chaudhary <vaishalichaudhary011@gmail.com>
2025-01-30 23:58:23 -0500

    Refactor: update the boilerplate

---
+++ my-ao-db/process.lua
@@ -0,0 +1,16 @@
+local process = { _version = "0.0.1" }
+ao = require('ao')
+
+function process.handle(msg, env) 
+
+  if (msg.Data == "ping") then
+    ao.send({ Target = msg.From, Data = "pong" })
+  end
+  
+  return ao.result({
+    Output = 'sent pong reply'
+  })
+
+end
+
+return process
